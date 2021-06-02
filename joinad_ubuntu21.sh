97c30f839967b4d6acf6ae0bf5bac9e60db2c2c3
amcamillo <58806811+amcamillo@users.noreply.github.com>
2021-06-02 12:41:50 +1200

    Update joinad_ubuntu21.sh

--- joinad_ubuntu21.sh
+++ joinad_ubuntu21.sh
@@ -2,6 +2,7 @@ echo "Starting Script"
 echo "1. Installing required packages"
 sudo apt -y install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
 
+echo ""
 echo "2. Joining AD domain"
 echo "Please enter the domain you wish to join:"
 read -r DOMAIN
@@ -12,14 +13,16 @@ echo ""
 
 sudo realm join $DOMAIN
 
+echo ""
 echo "3. Verifying it's Possible to Authenticate with the Domain"
 echo "Please enter domainusername@domain that you wish to test"
 read -r USER
 id $USER
 
+echo ""
 echo "4. Creating User environment"
 echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0022" | sudo tee -a /etc/pam.d/common-session
 
+echo ""
 echo "5. It's all done, sign out and try to login to make sure it works."
-fi
 exit
